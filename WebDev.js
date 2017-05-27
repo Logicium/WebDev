15459c4500fcdd33ac563fdb3aed69b0d9c79d40
Kisora Thomas <kisora.thomas@gmail.com>
2017-05-27 16:17:27 -0400

    Update

--- WebDev.js
+++ WebDev.js
@@ -1,6 +1,11 @@
+var bodyParser = require('body-parser');
 const express = require('express');
 var path    = require("path");
 const app = express();
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+app.use("/public", express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/public/WebDev.html'));
